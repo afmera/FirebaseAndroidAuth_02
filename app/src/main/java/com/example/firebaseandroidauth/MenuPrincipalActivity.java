73e5d950d83d174871d499a03355b9860204a37a
felipe <felipe@gmail.com>
2019-10-29 21:04:20 -0500

    Botones

--- app/src/main/java/com/example/firebaseandroidauth/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/firebaseandroidauth/MenuPrincipalActivity.java
@@ -3,6 +3,8 @@ package com.example.firebaseandroidauth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.firebaseandroidauth.objeto.Persona;
@@ -13,6 +15,7 @@ import java.util.List;
 public class MenuPrincipalActivity extends AppCompatActivity {
     //Genero un Objeto de la Layout.
     TextView textView_NombreUsuario;
+    Button btn_googlePlay,btn_qr,btn_geo,btn_config;
     //Genero una lista local de l clase determinda.
     List<Persona>lista;
     @Override
@@ -24,5 +27,35 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         lista= Lista.lista_persona;
         textView_NombreUsuario=(TextView)findViewById(R.id.NombreUsuarioTextView);
         textView_NombreUsuario.setText(lista.get(0).getNombreCompleto());
+
+        btn_googlePlay=(Button)findViewById(R.id.btnGooglePay);
+        btn_qr=(Button)findViewById(R.id.btnQR);
+        btn_geo=(Button)findViewById(R.id.btnGeoLocalizacion);
+        btn_config=(Button)findViewById(R.id.btnCofiguracion);
+        //Metodo por eventos del boton...
+        btn_googlePlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_geo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
